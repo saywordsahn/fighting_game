1e0b36050aaddd151d7f2cb1569c3291b654c769
saywordsahn <saywordsahn@gmail.com>
2025-01-14 10:41:10 +0900

    Added basic fighter class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from fighter import Fighter
 
 pygame.init()
 
@@ -9,10 +10,16 @@ bg_image = pygame.image.load('./assets/images/background/background.jpg')
 bg_image = pygame.transform.scale(bg_image, (WIDTH, HEIGHT))
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
+fighter1 = Fighter(200, 300)
+fighter2 = Fighter(700, 300)
+
 
 while True:
 
     screen.blit(bg_image, (0, 0))
+
+    fighter1.draw(screen)
+    fighter2.draw(screen)
 
     for event in pygame.event.get():
 
