efdb806855064638f417363e06fbda3f1fcacb51
saywordsahn <saywordsahn@gmail.com>
2025-01-22 13:59:02 +0900

    pre-lesson commit

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ pygame.init()
 
 WIDTH = 1000
 HEIGHT = 600
+YELLOW = (255, 255, 0)
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
 
 bg_image = pygame.image.load('./assets/images/background/background.jpg')
 bg_image = pygame.transform.scale(bg_image, (WIDTH, HEIGHT))
@@ -15,11 +18,18 @@ fighter2 = Fighter(700, 300)
 
 clock = pygame.time.Clock()
 
+def draw_health_bar(health, screen, x, y):
+    ratio = health / 100
+    pygame.draw.rect(screen, WHITE, (x, y, 400, 30))
+    pygame.draw.rect(screen, YELLOW, (x, y, 400 * ratio, 30))
+
 while True:
 
     dt = clock.tick(60) / 1000
 
     screen.blit(bg_image, (0, 0))
+    draw_health_bar(fighter1.health, screen, 20, 20)
+    draw_health_bar(fighter2.health, screen, 580, 20)
 
     fighter1.draw(screen)
     fighter2.draw(screen)
